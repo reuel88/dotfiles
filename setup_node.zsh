e13f407b2542120c7b2d90c901e0a1eae8d5f62d
Reuel Teodoro <12149910+reuel88@users.noreply.github.com>
2021-11-19 13:10:30 +1100

    Add setup_node.zsh and run with Dotbot

---
+++ setup_node.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting Node Setup >>>\n"
+
+if exists node; then
+  echo "Node $(node --version) & NPM $(npm --version) already installed"
+else
+  echo "Installing Node & NPM with NVM"
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+fi
